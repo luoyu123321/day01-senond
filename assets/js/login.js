79b4868ca26260dfcfd5e7a9ce3e74c2e5cb6176
luoyu123321 <779231460@qq.com>
2020-10-26 20:05:32 +0800

    完成表单验证

--- assets/js/login.js
+++ assets/js/login.js
@@ -8,3 +8,16 @@ $('#go-login').on('click', function(e) {
     $('#form-login').show()
     $('#form-reg').hide()
 })
+
+var form = layui.form
+form.verify({
+    pwd: [
+        /^[\S]{6,12}$/, '密码必须6到12位，且不能出现空格'
+    ],
+    repwd: function(value) {
+        var pwd = $('#form-reg input[name=username]').val()
+        if (value !== pwd) {
+            return '两次输入密码不一致'
+        }
+    }
+})
