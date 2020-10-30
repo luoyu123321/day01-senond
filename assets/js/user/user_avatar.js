c9f9a0f3e9f687fa8d453235a603876e3b2e8530
luoyu123321 <779231460@qq.com>
2020-10-30 20:48:59 +0800

    完成个人中心功能

--- assets/js/user/user_avatar.js
+++ assets/js/user/user_avatar.js
@@ -1,39 +1,58 @@
 $(function() {
 
-    var form = layui.form
-    var layer = layui.layer
-        // /my/updatepwd
-    form.verify({
-        pwd: [
-            /^[\S]{6,12}$/, '密码必须6到12位，且不能出现空格'
-        ],
-        samepwd: function(value) {
-            if (value === $('[name=oldPwd]').val()) {
-                return '新旧密码不能相同'
-            }
-        },
-        repwd: function(value) {
-            if (value !== $('[name=newPwd]').val()) {
-                return '两次密码输入不一致'
-            }
-        }
-    })
+    // 1.1 获取裁剪区域的 DOM 元素
+    var $image = $('#image')
+        // 1.2 配置选项
+    const options = {
+        // 纵横比
+        aspectRatio: 1,
+        // 指定预览区域
+        preview: '.img-preview'
+    }
 
-    $('.layui-form').on('submit', function(e) {
-        e.preventDefault()
+    // 1.3 创建裁剪区域
+    $image.cropper(options)
+
+    $('#btnUpdata').on('click', function() {
+        $('#file').click()
+    })
+    $('#file').on('change', function() {
+        var file = this.files[0]
+        if (file === undefined) {
+            return layui.layer.msg('清选择图片')
+        }
+        var fileUrl = URL.createObjectURL(file)
+
+        $image
+            .cropper('destroy') // 销毁旧的裁剪区域
+            .attr('src', fileUrl) // 重新设置图片路径
+            .cropper(options) // 重新初始化裁剪区域
+    })
+    $('#btnUpload').on('click', function() {
+        var dataURL = $image
+            .cropper('getCroppedCanvas', {
+                // 创建一个 Canvas 画布
+                width: 100,
+                height: 100
+            })
+            .toDataURL('image/png')
+
         $.ajax({
             method: 'POST',
-            url: '/my/updatepwd',
-            data: $(this).serialize(),
+            url: '/my/update/avatar',
+            data: {
+                avatar: dataURL
+            },
             success: function(res) {
                 if (res.status !== 0) {
-                    return layer.msg(res.message)
+                    return layui.layer.msg(res.message)
                 }
-                layer.msg(res.message)
-                $('.layui-form')[0].reset()
+                layui.layer.msg(res.message)
+                window.parent.getUserInfo()
             }
-
         })
     })
+
+
 
 })
